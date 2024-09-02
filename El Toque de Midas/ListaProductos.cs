adcfbbd0b43aa2771331e6e7db5e459a17acd07f
dgt2290 <dtrangoni@assertsolutions.com>
2024-09-02 18:07:22 -0300

    Lista de Productos

---
+++ El Toque de Midas/ListaProductos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace El_Toque_de_Midas
+{
+    public partial class ListaProductos : Form
+    {
+        public ListaProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListaProductos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
