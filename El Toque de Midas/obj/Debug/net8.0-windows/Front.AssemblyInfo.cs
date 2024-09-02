c62ad29d7a58d757a8168d2bca78e24b78ac110a
dgt2290 <dtrangoni@assertsolutions.com>
2024-09-02 18:38:49 -0300

    no sé qué onda

--- El Toque de Midas/obj/Debug/net8.0-windows/Front.AssemblyInfo.cs
+++ El Toque de Midas/obj/Debug/net8.0-windows/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f5c6a8c6eb3fe7fad18c84f4526bcfa781b959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448d58db2684392b7472645acdb7e2a73e49b65b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
