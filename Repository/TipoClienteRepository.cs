2d4486aa85e95c74e030c07d9acfbecacca5e430
Eduhardrock <gimenodanieleduardo@gmail.com>
2024-09-28 19:20:02 -0300

    Se realiza crud de tipo Cliente

---
+++ Repository/TipoClienteRepository.cs
@@ -0,0 +1,75 @@
+﻿using ModelORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class TipoClienteRepository
+    {
+
+        public tipoCliente crear(tipoCliente nuevotipoCliente)
+        {
+            using (midasTouchEntities db = new midasTouchEntities())
+            {
+                nuevotipoCliente = db.tipoClientes.Add(nuevotipoCliente);
+                db.SaveChanges();
+                return nuevotipoCliente;
+            }
+        }
+
+
+        public tipoCliente buscarPorId(int id)
+        {
+            using (midasTouchEntities db = new midasTouchEntities())
+            {
+                return db.tipoClientes.Find(id);
+            }
+        }
+
+        public tipoCliente buscar(string nombretipoCliente)
+        {
+            using (midasTouchEntities db = new midasTouchEntities())
+            {
+                return db.tipoClientes
+                              .Where(c => c.nombre == nombretipoCliente)
+                              .FirstOrDefault<tipoCliente>();
+            }
+        }
+
+
+        public List<tipoCliente> listarTodas()
+        {
+            using (midasTouchEntities db = new midasTouchEntities())
+            {
+                return db.tipoClientes.ToList();
+            }
+        }
+
+
+        public tipoCliente eliminar(int id)
+        {
+            using (midasTouchEntities db = new midasTouchEntities())
+            {
+                var tipoClienteBd = db.tipoClientes.Find(id);
+                db.tipoClientes.Remove(tipoClienteBd);
+                db.SaveChanges();
+                return tipoClienteBd;
+            }
+        }
+
+
+        public tipoCliente actualizar(int id, string nombre)
+        {
+            using (midasTouchEntities db = new midasTouchEntities())
+            {
+                var tipoClienteBD = db.tipoClientes.Find(id);
+                tipoClienteBD.nombre = nombre;
+                db.SaveChanges();
+                return tipoClienteBD;
+            }
+        }
+    }
+}
---
+++ Service/TipoClienteService.cs
@@ -0,0 +1,70 @@
+﻿using ModelORM;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class TipoClienteService
+    {
+
+        private TipoClienteRepository tipoClienteRepository = new TipoClienteRepository();
+
+        public tipoCliente crear(tipoCliente nuevotipoCliente)
+        {
+            Console.WriteLine("Entre a crear tipoCliente");
+            var cat = this.buscar(nuevotipoCliente.nombre);
+
+            Console.WriteLine("Resultado de buscar la tipoCliente en la bd : " + cat.id);
+            if (cat != null)
+            {
+                throw new Exception("Ya existe un tipoCliente con el nombre : " + nuevotipoCliente.nombre);
+            }
+            return this.tipoClienteRepository.crear(nuevotipoCliente);
+        }
+
+
+        public tipoCliente buscar(string nombrtipoCliente)
+        {
+            return this.tipoClienteRepository.buscar(nombrtipoCliente);
+        }
+
+        public tipoCliente buscarPorId(int id)
+        {
+            return this.tipoClienteRepository.buscarPorId(id);
+        }
+
+
+        public List<tipoCliente> listarTodas()
+        {
+            return this.tipoClienteRepository.listarTodas();
+        }
+
+
+        public tipoCliente eliminar(int id)
+        {
+            var cat = this.buscarPorId(id);
+            if (cat == null)
+            {
+                throw new Exception("La tipoCliente que intenta eliminar no existe en la base de datos.");
+            }
+
+            return this.tipoClienteRepository.eliminar(id);
+        }
+
+
+        public tipoCliente actualizar(int id, string nombre)
+        {
+            var cat = this.buscarPorId(id);
+            if (cat == null)
+            {
+                throw new Exception("No existe ningun tipoCliente con el id brindado. Verificar");
+            }
+
+            return this.tipoClienteRepository.actualizar(id, nombre);
+        }
+    }
+}
